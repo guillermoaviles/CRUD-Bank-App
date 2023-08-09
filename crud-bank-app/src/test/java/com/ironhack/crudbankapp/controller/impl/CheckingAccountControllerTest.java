48130e4358433ae94e6b96a794d044a99f72121b
Guillermo Aviles <gjaviles7@gmail.com>
2023-08-09 11:30:07 -0400

    updated test file

--- crud-bank-app/src/test/java/com/ironhack/crudbankapp/controller/impl/CheckingAccountControllerTest.java
+++ crud-bank-app/src/test/java/com/ironhack/crudbankapp/controller/impl/CheckingAccountControllerTest.java
@@ -1,10 +1,22 @@
 package com.ironhack.crudbankapp.controller.impl;
 
+import com.ironhack.crudbankapp.repository.CheckingAccountRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class CheckingAccountControllerTest {
+
+    @Autowired
+    CheckingAccountRepository checkingAccountRepository;
+
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
 
     @Test
     void saveCheckingAccount() {
