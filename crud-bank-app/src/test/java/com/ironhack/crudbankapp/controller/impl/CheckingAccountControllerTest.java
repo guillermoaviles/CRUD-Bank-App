8258fa351c7b9f5d56013d33349570b3fdaa76bd
Guillermo Aviles <gjaviles7@gmail.com>
2023-08-09 12:42:37 -0400

    saveCheckingAccount_validBody_checkingAccountSaved

--- crud-bank-app/src/test/java/com/ironhack/crudbankapp/controller/impl/CheckingAccountControllerTest.java
+++ crud-bank-app/src/test/java/com/ironhack/crudbankapp/controller/impl/CheckingAccountControllerTest.java
@@ -1,12 +1,21 @@
 package com.ironhack.crudbankapp.controller.impl;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ironhack.crudbankapp.model.CheckingAccount;
 import com.ironhack.crudbankapp.repository.CheckingAccountRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class CheckingAccountControllerTest {
@@ -17,13 +26,43 @@ class CheckingAccountControllerTest {
     @Autowired
     WebApplicationContext webApplicationContext;
 
+    MockMvc mockMvc;
 
-    @Test
-    void saveCheckingAccount() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    CheckingAccount checkingAccount1;
+    CheckingAccount checkingAccount2;
+    CheckingAccount checkingAccount3;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        checkingAccount1 = new CheckingAccount("Guillermo Aviles");
+        checkingAccount2 = new CheckingAccount("Sofia Mendez");
+        checkingAccount3 = new CheckingAccount("Bayi Aviles");
+    }
+
+    @AfterEach
+    void tearDown() {
+        checkingAccountRepository.deleteById(1);
+        checkingAccountRepository.deleteById(2);
+        checkingAccountRepository.deleteById(3);
     }
 
     @Test
-    void updateCheckingAccount() {
+    void saveCheckingAccount_validBody_checkingAccountSaved() throws Exception {
+        String body = objectMapper.writeValueAsString(checkingAccount1);
+
+        mockMvc.perform(post("/api/accounts/checking").content(body).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andReturn();
+
+        assertTrue(checkingAccountRepository.findAll().toString().contains("Guillermo Aviles"));
+    }
+
+    @Test
+    void updateCheckingAccount_validBody_checkingAccountUpdated() {
+
     }
 
     @Test
